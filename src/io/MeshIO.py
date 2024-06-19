0fe8408dac0eefad041dc747c43587f3e6ef9f45
Roman Fries <r.fries@stud.unibas.ch>
2024-06-19 17:10:19 +0200

    Initial project structure: First major task: Importing .stl/.ply files

---
+++ src/io/MeshIO.py
@@ -0,0 +1,7 @@
+class MeshIO:
+    def __init__(self, file):
+        self.file = file
+
+    def read_mesh(self):
+        # TODO: Read .stl/.ply files
+        return
---
+++ src/main.py
@@ -0,0 +1,12 @@
+from io.MeshIO import MeshIO
+class Main:
+    def __init__(self):
+        print("Hello, world! This is the main class.")
+
+    def run(self):
+        print("Running the main class.")
+
+
+if __name__ == "__main__":
+    main = Main()
+    main.run()
