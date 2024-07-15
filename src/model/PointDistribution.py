Implemented: - Save the complete Markov chain in the random walk proposal - Fixed the calculation of the model parameters by multiplying the eigenvectors by the square-root of the corresponding eigenvalue - Implemented an idea to get one-to-one point correspondences - Enhanced the visualizer

src/model/PointDistribution.py
@@ -19,8 +19,8 @@ def apply_svd(centered_points, num_components):
     return np.square(s) / (num_components - 1), np.transpose(V_T)
 
 
-def get_parameters(stacked_points, eigenvectors):
-    parameters, residuals, rank, s = np.linalg.lstsq(eigenvectors, stacked_points, rcond=None)
+def get_parameters(stacked_points, components):
+    parameters, residuals, rank, s = np.linalg.lstsq(components, stacked_points, rcond=None)
     return parameters
 
 
@@ -37,7 +37,7 @@ def batch_multivariate_gaussian_pdf(k, points, mean, covariance):
     det = torch.det(covariance)
     inv = torch.inverse(covariance).double()
     normalization = 1.0 / torch.sqrt(torch.pow(torch.tensor(2 * torch.pi), float(k)) * det)
-    points_centered = points - mean
+    points_centered = (points - mean).double()
     exponent = -0.5 * torch.einsum('ijk,jl,ilk->ik', points_centered, inv, points_centered)
     return normalization * torch.exp(exponent)
 
@@ -72,8 +72,9 @@ class PointDistributionModel:
         self.sample_size = self.stacked_points.shape[1]
         # Eigenvectors are the columns of the 2-dimensional ndarray 'self.eigenvectors'
         self.eigenvalues, self.eigenvectors = apply_svd(self.points_centered, self.sample_size)
+        self.components = self.eigenvectors * np.sqrt(self.eigenvalues)
         # self.parameters = get_parameters(self.points_centered, self.eigenvectors)
-        self.parameters = get_parameters(self.points_centered, self.eigenvectors)
+        self.parameters = get_parameters(self.points_centered, self.components)
 
     def get_eigenvalues(self):
         return self.eigenvalues
@@ -81,19 +82,25 @@ class PointDistributionModel:
     def get_eigenvalue_k(self, k):
         return self.eigenvalues[k]
 
-    def get_eigenvectors(self):
-        return self.eigenvectors
+    def get_components(self):
+        return self.components
 
-    def get_eigenvector_k(self, k):
-        return self.eigenvectors[:, k]
+    def get_component_k(self, k):
+        return self.components[:, k]
 
     def get_points_from_parameters(self, parameters):
-        stacked_points = np.transpose(parameters) @ np.transpose(self.eigenvectors)
-        return stacked_points.reshape((-1, 3))
+        if parameters.ndim == 1:
+            parameters = parameters[:, np.newaxis]
+        batch_size = parameters.shape[1]
+        stacked_points = self.components @ parameters + self.mean
+        if batch_size == 1:
+            return stacked_points.reshape((-1, 3))
+        else:
+            return stacked_points.reshape((-1, 3, batch_size))
 
 
 class PDMMetropolisSampler:
-    def __init__(self, pdm, proposal, batch_mesh, target, correspondences=True, sigma_lm=10.0, sigma_prior=10000.0):
+    def __init__(self, pdm, proposal, batch_mesh, target, correspondences=True, sigma_lm=0.1, sigma_prior=1.0):
         self.model = pdm
         self.proposal = proposal
         self.batch_mesh = batch_mesh
@@ -114,9 +121,7 @@ class PDMMetropolisSampler:
         self.proposal.propose()
 
     def update_mesh(self):
-        reconstructed_points = self.model.get_eigenvectors() @ self.proposal.get_parameters().numpy() + self.model.mean
-        reconstructed_points = reconstructed_points.reshape((self.batch_mesh.num_points, self.batch_mesh.dimensionality,
-                                                             self.proposal.batch_size))
+        reconstructed_points = self.model.get_points_from_parameters(self.proposal.get_parameters().numpy())
         self.batch_mesh.set_points(reconstructed_points, save_old=True)
         self.points = self.batch_mesh.tensor_points
 
@@ -134,12 +139,13 @@ class PDMMetropolisSampler:
 
     def decide(self):
         # log-ratio!
-        ratio = self.old_posterior / self.posterior
+        ratio = torch.exp(self.posterior - self.old_posterior)
         probabilities = torch.min(ratio, torch.ones_like(ratio))
         randoms = torch.rand(self.batch_size)
         decider = torch.gt(probabilities, randoms)
+        # decider = torch.ones(self.batch_size).bool()
 
-        self.proposal.update_parameters(decider)
+        self.proposal.update(decider)
         self.batch_mesh.update_points(decider)
         self.points = self.batch_mesh.tensor_points
 

src/registration/Procrustes.py
@@ -45,7 +45,8 @@ class ProcrustesAnalyser:
             # clarifies to which mesh these landmarks belong.
             self.reference, self.landmarks_set, self.identifiers = prepare_landmark_json(reference, landmarks_set)
         elif data_format == 'meshio_mesh':
-            # Important: Only functions as desired if correspondences are given
+            # Important: Only functions as desired if correspondences are given and should therefore be used with
+            # caution!
             self.landmarks_set = []
             self.identifiers = []
             for mesh in landmarks_set:
@@ -53,8 +54,7 @@ class ProcrustesAnalyser:
                 self.identifiers.append(mesh.id)
             self.reference = self.landmarks_set[0]
         else:
-            # TODO
-            pass
+            raise NotImplementedError('Procrustes analysis not implemented for this data format.')
         self.data_format = data_format
         self.iterations = iterations
         self.cardinality = len(landmarks_set)
@@ -72,8 +72,15 @@ class ProcrustesAnalyser:
         transforms = [np.matmul(a, b) for a, b in zip(transforms, new_transforms)]
         # The initial reference is not used any further!
 
+        # Reinsert the reference
+        if self.data_format == 'landmark_list':
+            transforms.append(np.identity(4))
+            new_transforms.append(np.identity(4))
+            new_landmarks.append(np.array(self.reference))
+            self.identifiers.append('reference')
+
         # 3: Compute the mean shape \Gamma_{\mu} for the set of aligned shapes
-        for j in range(self.iterations):
+        for j in range(self.iterations - 1):
             mean_shape = calculate_mean_shape(new_landmarks)
 
             # 4: Iterate using the mean shape as the new reference shape

src/sampling/proposals/GaussRandWalk.py
@@ -4,7 +4,7 @@ import torch
 
 class GaussianRandomWalkProposal:
 
-    def __init__(self, batch_size, starting_parameters, sigma=5.0):
+    def __init__(self, batch_size, starting_parameters, sigma=0.3, chain_length_step=1000):
         """
         The class is used to draw new values for the theta parameters.
         A whole batch of new suggestions is always generated.
@@ -25,6 +25,10 @@ class GaussianRandomWalkProposal:
         self.sigma = sigma
         self.old_parameters = None
 
+        self.chain_length = 0
+        self.chain_length_step = chain_length_step
+        self.chain = torch.zeros((self.num_parameters, self.batch_size, 0))
+
     def propose(self):
         """
         Updates the parameter values using a Gaussian random walk (see class description).
@@ -34,6 +38,8 @@ class GaussianRandomWalkProposal:
         perturbations = torch.randn((self.num_parameters, self.batch_size))
         self.old_parameters = self.parameters
         self.parameters = self.parameters + perturbations * self.sigma
+        # self.parameters = torch.zeros((self.num_parameters, self.batch_size))
+        # self.parameters = perturbations * self.sigma
 
     def get_parameters(self):
         """
@@ -44,7 +50,23 @@ class GaussianRandomWalkProposal:
         """
         return self.parameters
 
+    def update(self, decider):
+        self.update_parameters(decider)
+        self.update_chain()
+
     def update_parameters(self, decider):
         self.parameters = torch.where(decider.unsqueeze(0), self.parameters,
                                       self.old_parameters)
         self.old_parameters = None
+
+    def update_chain(self):
+        if self.chain_length % self.chain_length_step == 0:
+            self.extend_chain()
+
+        self.chain[:, :, self.chain_length] = self.parameters
+        self.chain_length += 1
+
+    def extend_chain(self):
+        updated_chain = torch.zeros((self.num_parameters, self.batch_size, self.chain_length + self.chain_length_step))
+        updated_chain[:, :, :self.chain_length] = self.chain
+        self.chain = updated_chain

src/visualization/DashViewer.py
@@ -5,10 +5,11 @@ import plotly.graph_objects as go
 
 
 class MeshVisualizer:
-    def __init__(self, meshes):
+    def __init__(self, app, meshes):
+        self.app = app
         self.meshes = meshes
-        self.app = dash.Dash(__name__)
         self.length = len(meshes)
+        self.run()
 
     def run(self):
         concatenated_points = np.concatenate([mesh.points.flatten() for mesh in self.meshes])
@@ -69,21 +70,19 @@ class MeshVisualizer:
 
             return mesh_figure
 
-        self.app.run_server(host='localhost', port=8005)
 
-
-class ProposalVisualizer:
-    def __init__(self, mesh):
+class BatchMeshVisualizer:
+    def __init__(self, app, mesh):
+        self.app = app
         self.mesh = mesh
-        self.app = dash.Dash(__name__)
         self.length = mesh.batch_size
+        self.layout = self.setup_layout_and_callbacks()
 
-    def run(self):
+    def setup_layout_and_callbacks(self):
         display_range = [np.min(self.mesh.points), np.max(self.mesh.points)]
 
-
         # Define the layout of the Dash app
-        self.app.layout = html.Div([
+        layout = html.Div([
             html.Div([
                 html.H1("3D Mesh Visualization", style={'textAlign': 'center', 'marginTop': '10px'}),
                 dcc.Slider(
@@ -109,7 +108,6 @@ class ProposalVisualizer:
             Output('3d-mesh', 'figure'),
             Input('custom-slider', 'value'))
         def update_figure(mesh_to_show):
-
             x, y, z = self.mesh.tensor_points[:, :, mesh_to_show].T
             i, j, k = self.mesh.cells[0].data.T
             mesh_figure = go.Figure(data=[
@@ -128,12 +126,121 @@ class ProposalVisualizer:
             mesh_figure.update_layout(
                 scene=dict(
                     aspectmode='cube',
-                    xaxis=dict(nticks=100, range=display_range),
-                    yaxis=dict(nticks=100, range=display_range),
-                    zaxis=dict(nticks=100, range=display_range),
+                    xaxis=dict(nticks=10, range=display_range),
+                    yaxis=dict(nticks=10, range=display_range),
+                    zaxis=dict(nticks=10, range=display_range),
                 )
             )
 
             return mesh_figure
 
-        self.app.run_server(host='localhost', port=8005)
+        return layout
+
+
+class ModelVisualizer:
+    def __init__(self, app, model):
+        self.app = app
+        self.model = model
+        self.num_parameters = self.model.sample_size
+        self.parameters = np.zeros(self.num_parameters)
+        self.layout = self.setup_layout_and_callbacks()
+
+    def setup_layout_and_callbacks(self):
+        display_range = [np.min(self.model.meshes[0].points), np.max(self.model.meshes[0].points)]
+        sliders = []
+        for i in range(self.num_parameters):
+            sliders.append(html.Label(f'Parameter {i + 1}', style={'font-size': '10px'}))
+            sliders.append(
+                dcc.Slider(
+                    id=f'param-{i}',
+                    min=-5,
+                    max=5,
+                    step=0.01,
+                    value=0,
+                    marks={-5: '-5', 0: '0', 5: '5'},
+                    tooltip={"placement": "bottom", "always_visible": True},
+                    updatemode='drag',
+                )
+            )
+
+        layout = html.Div([
+            html.H1("3D Mesh Viewer", style={'text-align': 'center'}),
+            html.Div([
+                dcc.Graph(id='mesh-plot', style={'width': '100%', 'height': '80vh', 'display': 'inline-block'}),
+                html.Div(sliders,
+                         style={'width': '20%', 'display': 'inline-block', 'vertical-align': 'top', 'padding': '10px'})
+            ])
+        ])
+
+        inputs = [Input(f'param-{i}', 'value') for i in range(self.num_parameters)]
+
+        @callback(
+            Output('mesh-plot', 'figure'),
+            inputs
+        )
+        def update_mesh(*parameters):
+            params = np.asarray(parameters)
+            x, y, z = np.transpose(self.model.get_points_from_parameters(params))
+            i, j, k = self.model.meshes[0].cells[0].data.T
+            mesh_figure = go.Figure(data=[
+                go.Mesh3d(
+                    x=x,
+                    y=y,
+                    z=z,
+                    i=i,
+                    j=j,
+                    k=k,
+                    color='lightpink',
+                    opacity=0.50
+                )
+            ])
+
+            mesh_figure.update_layout(
+                scene=dict(
+                    aspectmode='cube',
+                    xaxis=dict(nticks=10, range=display_range),
+                    yaxis=dict(nticks=10, range=display_range),
+                    zaxis=dict(nticks=10, range=display_range),
+                )
+            )
+
+            return mesh_figure
+
+        return layout
+
+
+class MainVisualizer:
+    def __init__(self, mesh, model):
+        self.app = dash.Dash(__name__, suppress_callback_exceptions=True)
+        self.batch_mesh_visualizer = BatchMeshVisualizer(self.app, mesh)
+        self.model_visualizer = ModelVisualizer(self.app, model)
+        self.setup_layout()
+        self.setup_callbacks()
+
+    def setup_layout(self):
+        self.app.layout = html.Div([
+            dcc.Dropdown(
+                id='scene-dropdown',
+                options=[
+                    {'label': 'Batch Mesh', 'value': 'scene1'},
+                    {'label': 'Model', 'value': 'scene2'}
+                ],
+                value='scene1'
+            ),
+            html.Div(id='scene-container')
+        ])
+
+    def setup_callbacks(self):
+        @self.app.callback(
+            Output('scene-container', 'children'),
+            [Input('scene-dropdown', 'value')]
+        )
+        def display_scene(scene):
+            if scene == 'scene1':
+                return self.batch_mesh_visualizer.layout
+            elif scene == 'scene2':
+                return self.model_visualizer.layout
+            return html.Div()
+
+    def run(self):
+        self.app.run_server(debug=True)