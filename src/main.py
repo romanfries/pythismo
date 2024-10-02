Implemented: - Likelihood calculation independent of the number of points in the model - Small bugfix for the Markov chain visualiser.

src/main.py
@@ -25,10 +25,10 @@ REL_PATH_MESH = "datasets/femur-data/project-data/registered"
 REL_PATH_MESH_DECIMATED = "datasets/femur-data/project-data/registered-decimated"
 REL_PATH_MODEL = "datasets/femur-data/project-data/models"
 REL_PATH_REFERENCE = "datasets/femur-data/project-data/reference-decimated"
-REL_PATH_INPUT_OUTPUT = "datasets/femur-data/project-data/output/toy"
+REL_PATH_INPUT_OUTPUT = "datasets/femur-data/project-data/output/toy-sigma/statistics/sigma_200"
 
 BATCH_SIZE = 20
-CHAIN_LENGTH = 22000
+CHAIN_LENGTH = 2000
 DECIMATION_TARGET = 200
 
 MODEL_PROBABILITY = 0.6
@@ -38,16 +38,19 @@ ROTATION_PROBABILITY = 0.2
 PROPOSAL_TYPE = "CP_SIMPLE"
 
 SIGMA_MOD_GAUSS = torch.tensor([0.04, 0.08, 0.16], device=DEVICE)
-SIGMA_MOD_CP = torch.tensor([0.1, 0.2, 0.4], device=DEVICE)
-SIGMA_TRANS = torch.tensor([0.15, 0.3, 0.6], device=DEVICE)
+SIGMA_MOD_CP = torch.tensor([0.15, 0.3, 0.6], device=DEVICE)
+SIGMA_TRANS = torch.tensor([0.3, 0.6, 1.2], device=DEVICE)
 # Variance in radians
-SIGMA_ROT = torch.tensor([0.001, 0.002, 0.004], device=DEVICE)
+SIGMA_ROT = torch.tensor([0.002, 0.004, 0.008], device=DEVICE)
 PROB_MOD = PROB_TRANS = PROB_ROT = torch.tensor([0.2, 0.6, 0.2], device=DEVICE)
 
 UNIFORM_POSE_PRIOR = False
 # These two parameters are irrelevant when selecting a uniform pose prior
 SIGMA_PRIOR_TRANS = 30.0
 SIGMA_PRIOR_ROT = 0.01
+
+SIGMA_LIKELIHOOD_TERM = [1.0]
+GAMMA = 50.0
 
 CP_D = 1.0
 CP_RECALCULATION_PERIOD = 1000
@@ -75,6 +78,7 @@ def trial():
     loo = torch.randint(0, len(meshes), (1,)).item()
     # obs = PERCENTAGES_OBSERVED_LENGTH[torch.randint(0, len(PERCENTAGES_OBSERVED_LENGTH), (1,)).item()]
     obs = 0.2
+    sigma_likelihood = 1.0
     target = meshes[loo]
     del meshes[loo]
     z_min, z_max = torch.min(target.tensor_points, dim=0)[1][2].item(), \
@@ -99,8 +103,8 @@ def trial():
                                            batched_target, model, SIGMA_MOD_CP, SIGMA_TRANS, SIGMA_ROT, PROB_MOD,
                                            PROB_TRANS, PROB_ROT, CP_D, CP_RECALCULATION_PERIOD)
     sampler = PDMMetropolisSampler(model, random_walk, batched_shape, batched_target, correspondences=False,
-                                   uniform_pose_prior=UNIFORM_POSE_PRIOR, sigma_trans=SIGMA_PRIOR_TRANS,
-                                   sigma_rot=SIGMA_PRIOR_ROT)
+                                   gamma=GAMMA, sigma_lm=sigma_likelihood, uniform_pose_prior=UNIFORM_POSE_PRIOR,
+                                   sigma_trans=SIGMA_PRIOR_TRANS, sigma_rot=SIGMA_PRIOR_ROT)
 
     generator = torch.Generator(device=DEVICE)
     for _ in tqdm(range(CHAIN_LENGTH)):
@@ -195,61 +199,63 @@ def loocv():
     # LOOCV (Leave-One-Out Cross-Validation) procedure
     handler = DataHandler(REL_PATH_INPUT_OUTPUT)
     meshes, _ = read_meshes(REL_PATH_MESH_DECIMATED, DEVICE)
-    for percentage in PERCENTAGES_OBSERVED_LENGTH:
-        # for l_ in range(len(meshes)):
-        for l_ in range(10):
-            target = meshes[l_]
-            del meshes[l_]
-            z_min, z_max = torch.min(target.tensor_points, dim=0)[1][2].item(), \
-                torch.max(target.tensor_points, dim=0)[1][2].item()
-            part_target, plane_normal, plane_origin = target.partial_shape(z_max, z_min, percentage)
-            meshes.insert(0, part_target)
-            ICPAnalyser(meshes).icp()
-            del meshes[0]
-            model = PointDistributionModel(meshes=meshes)
-            shape = meshes[0]
-            dists = distance_to_closest_point(target.tensor_points.unsqueeze(-1), part_target.tensor_points, 1)
-            observed = (dists < 1e-6).squeeze()
-            batched_target = BatchTorchMesh(part_target, 'target', DEVICE, BATCH_SIZE)
-            starting_params = torch.zeros(model.rank, device=DEVICE)
-            shape.set_points(model.get_points_from_parameters(starting_params), reset_com=True)
-            batched_shape = BatchTorchMesh(shape, 'current_shapes', DEVICE, BATCH_SIZE)
-            if PROPOSAL_TYPE == "GAUSS_RAND":
-                random_walk = GaussianRandomWalkProposal(BATCH_SIZE, starting_params, DEVICE, SIGMA_MOD_GAUSS,
-                                                         SIGMA_TRANS, SIGMA_ROT, PROB_MOD, PROB_TRANS, PROB_ROT)
-            elif PROPOSAL_TYPE == "CP_SIMPLE":
-                random_walk = ClosestPointProposal(BATCH_SIZE, starting_params, DEVICE, batched_shape,
-                                                   batched_target, model, SIGMA_MOD_CP, SIGMA_TRANS, SIGMA_ROT,
-                                                   PROB_MOD, PROB_TRANS, PROB_ROT, CP_D, CP_RECALCULATION_PERIOD)
-            sampler = PDMMetropolisSampler(model, random_walk, batched_shape, batched_target, correspondences=False,
-                                           uniform_pose_prior=UNIFORM_POSE_PRIOR, sigma_trans=SIGMA_PRIOR_TRANS,
-                                           sigma_rot=SIGMA_PRIOR_ROT, save_full_mesh_chain=True,
-                                           save_residuals=True)
+    for sigma_likelihood in SIGMA_LIKELIHOOD_TERM:
+        for percentage in PERCENTAGES_OBSERVED_LENGTH:
+            for l_ in range(len(meshes)):
+            # for l_ in range(10):
+                target = meshes[l_]
+                del meshes[l_]
+                z_min, z_max = torch.min(target.tensor_points, dim=0)[1][2].item(), \
+                    torch.max(target.tensor_points, dim=0)[1][2].item()
+                part_target, plane_normal, plane_origin = target.partial_shape(z_max, z_min, percentage)
+                meshes.insert(0, part_target)
+                ICPAnalyser(meshes).icp()
+                del meshes[0]
+                model = PointDistributionModel(meshes=meshes)
+                shape = meshes[0]
+                dists = distance_to_closest_point(target.tensor_points.unsqueeze(-1), part_target.tensor_points, 1)
+                observed = (dists < 1e-6).squeeze()
+                batched_target = BatchTorchMesh(part_target, 'target', DEVICE, BATCH_SIZE)
+                starting_params = torch.zeros(model.rank, device=DEVICE)
+                shape.set_points(model.get_points_from_parameters(starting_params), reset_com=True)
+                batched_shape = BatchTorchMesh(shape, 'current_shapes', DEVICE, BATCH_SIZE)
+                if PROPOSAL_TYPE == "GAUSS_RAND":
+                    random_walk = GaussianRandomWalkProposal(BATCH_SIZE, starting_params, DEVICE, SIGMA_MOD_GAUSS,
+                                                             SIGMA_TRANS, SIGMA_ROT, PROB_MOD, PROB_TRANS, PROB_ROT)
+                elif PROPOSAL_TYPE == "CP_SIMPLE":
+                    random_walk = ClosestPointProposal(BATCH_SIZE, starting_params, DEVICE, batched_shape,
+                                                       batched_target, model, SIGMA_MOD_CP, SIGMA_TRANS, SIGMA_ROT,
+                                                       PROB_MOD, PROB_TRANS, PROB_ROT, CP_D, CP_RECALCULATION_PERIOD)
+                sampler = PDMMetropolisSampler(model, random_walk, batched_shape, batched_target, correspondences=False,
+                                               gamma=GAMMA, sigma_lm=sigma_likelihood,
+                                               uniform_pose_prior=UNIFORM_POSE_PRIOR, sigma_trans=SIGMA_PRIOR_TRANS,
+                                               sigma_rot=SIGMA_PRIOR_ROT, save_full_mesh_chain=True,
+                                               save_residuals=True)
 
-            generator = torch.Generator(device=DEVICE)
-            for _ in tqdm(range(CHAIN_LENGTH)):
-                random = torch.rand(1, device=DEVICE, generator=generator).item()
-                if random < MODEL_PROBABILITY:
-                    proposal = ParameterProposalType.MODEL
-                elif MODEL_PROBABILITY <= random < MODEL_PROBABILITY + TRANSLATION_PROBABILITY:
-                    proposal = ParameterProposalType.TRANSLATION
-                else:
-                    proposal = ParameterProposalType.ROTATION
-                sampler.propose(proposal)
-                sampler.determine_quality(proposal)
-                sampler.decide(proposal, target)
-            random_walk.close()
+                generator = torch.Generator(device=DEVICE)
+                for _ in tqdm(range(CHAIN_LENGTH)):
+                    random = torch.rand(1, device=DEVICE, generator=generator).item()
+                    if random < MODEL_PROBABILITY:
+                        proposal = ParameterProposalType.MODEL
+                    elif MODEL_PROBABILITY <= random < MODEL_PROBABILITY + TRANSLATION_PROBABILITY:
+                        proposal = ParameterProposalType.TRANSLATION
+                    else:
+                        proposal = ParameterProposalType.ROTATION
+                    sampler.propose(proposal)
+                    sampler.determine_quality(proposal)
+                    sampler.decide(proposal, target)
+                random_walk.close()
 
-            analyser = ChainAnalyser(sampler, random_walk, model, observed, sampler.full_chain)
-            # analyser.detect_burn_in()
-            data = analyser.data_to_json(l_, int(100 * percentage))
-            meshes.insert(l_, target)
-            handler.write_statistics(data, l_, int(100 * percentage))
-            # Code to save further data
-            # chain_residual_dict = sampler.get_dict_chain_and_residuals()
-            # handler.write_chain_and_residuals(chain_residual_dict, l, int(100 * percentage))
-            # param_chain_posterior_dict = random_walk.get_dict_param_chain_posterior()
-            # handler.write_param_chain_posterior(param_chain_posterior_dict, l, int(100 * percentage))
+                analyser = ChainAnalyser(sampler, random_walk, model, observed, sampler.full_chain)
+                # analyser.detect_burn_in()
+                data = analyser.data_to_json(l_, int(100 * percentage))
+                meshes.insert(l_, target)
+                handler.write_statistics(data, l_, int(100 * percentage), int(100 * sigma_likelihood))
+                # Code to save further data
+                # chain_residual_dict = sampler.get_dict_chain_and_residuals()
+                # handler.write_chain_and_residuals(chain_residual_dict, l, int(100 * percentage))
+                # param_chain_posterior_dict = random_walk.get_dict_param_chain_posterior()
+                # handler.write_param_chain_posterior(param_chain_posterior_dict, l, int(100 * percentage))
 
 
 if __name__ == "__main__":

src/sampling/Metropolis.py
@@ -7,7 +7,7 @@ from pytorch3d.loss.point_mesh_distance import point_face_distance
 from src.sampling.proposals import ParameterProposalType
 
 
-def unnormalised_log_posterior(distances, parameters, translation, rotation, sigma_lm, sigma_mod,
+def unnormalised_log_posterior(distances, parameters, translation, rotation, gamma, sigma_lm, sigma_mod,
                                uniform_pose_prior=True, sigma_trans=20.0, sigma_rot=0.005):
     """
     Calculates the unnormalised log posterior given the distance of each point of the target to its closest point on the
@@ -30,6 +30,8 @@ def unnormalised_log_posterior(distances, parameters, translation, rotation, sig
     :param rotation: Tensor with the current rotation parameters of the current shapes considered with shape
     (3, batch_size).
     :type rotation: torch.Tensor
+    :param gamma: Multiplication factor of the likelihood term.
+    :type gamma: float
     :param sigma_lm: Variance of the zero-mean Gaussian, which is used to evaluate the L2 distances. The same variance
     value is used for all 3 dimensions (isotropic distribution).
     :type sigma_lm: float
@@ -46,7 +48,7 @@ def unnormalised_log_posterior(distances, parameters, translation, rotation, sig
     :rtype: torch.Tensor
     """
     distances_squared = torch.pow(distances, 2)
-    log_likelihoods = 0.5 * torch.sum(sigma_lm * (-distances_squared), dim=0)
+    log_likelihoods = 0.5 * gamma * torch.mean(sigma_lm * (-distances_squared), dim=0)
     log_prior = 0.5 * torch.sum(-torch.pow(parameters / sigma_mod, 2), dim=0)
     rotation = (rotation + math.pi) % (2.0 * math.pi) - math.pi
     if uniform_pose_prior:
@@ -59,9 +61,9 @@ def unnormalised_log_posterior(distances, parameters, translation, rotation, sig
 
 
 class PDMMetropolisSampler:
-    def __init__(self, pdm, proposal, batch_mesh, target, correspondences=True, sigma_lm=1.0, sigma_prior=1.0,
-                 uniform_pose_prior=True, sigma_trans=20.0, sigma_rot=0.005, save_full_mesh_chain=False,
-                 save_residuals=False):
+    def __init__(self, pdm, proposal, batch_mesh, target, correspondences=True, gamma=50.0, sigma_lm=1.0,
+                 sigma_prior=1.0, uniform_pose_prior=True, sigma_trans=20.0, sigma_rot=0.005,
+                 save_full_mesh_chain=False, save_residuals=False):
         """
         Main class for Bayesian model fitting using Markov Chain Monte Carlo (MCMC). The Metropolis algorithm
         allows the user to draw samples from any distribution, given that the unnormalized distribution can be evaluated
@@ -81,6 +83,8 @@ class PDMMetropolisSampler:
         :param correspondences: Boolean variable that determines whether there are known point correspondences between
         the model and the target.
         :type correspondences: bool
+        :param gamma: Multiplication factor of the likelihood term.
+        :type gamma: float
         :param sigma_lm: Variance used (in square millimetres) when determining the likelihood term of the posterior.
         For the likelihood term, the L2 distance (independent point evaluator likelihood) is used. The L2 distance is
         evaluated using a zero-mean Gaussian with variance sigma_lm.
@@ -111,6 +115,7 @@ class PDMMetropolisSampler:
         self.target_points = self.target.tensor_points
         self.correspondences = correspondences
         self.batch_size = self.proposal.batch_size
+        self.gamma = gamma
         self.sigma_lm = sigma_lm
         self.sigma_prior = sigma_prior
         self.uniform_pose_prior = uniform_pose_prior
@@ -185,7 +190,7 @@ class PDMMetropolisSampler:
             differences = torch.sub(self.points, self.target_points)
             distances = torch.linalg.vector_norm(differences, dim=1)
             posterior = unnormalised_log_posterior(distances, self.proposal.parameters, self.proposal.translation,
-                                                   self.proposal.rotation, self.sigma_lm, self.sigma_prior,
+                                                   self.proposal.rotation, self.gamma, self.sigma_lm, self.sigma_prior,
                                                    self.uniform_pose_prior, self.sigma_trans, self.sigma_rot)
 
         else:
@@ -211,9 +216,9 @@ class PDMMetropolisSampler:
 
             # Target is a point cloud, reference a mesh.
             posterior = unnormalised_log_posterior(torch.sqrt(point_to_face), self.proposal.parameters,
-                                                   self.proposal.translation, self.proposal.rotation, self.sigma_lm,
-                                                   self.sigma_prior, self.uniform_pose_prior, self.sigma_trans,
-                                                   self.sigma_rot)
+                                                   self.proposal.translation, self.proposal.rotation, self.gamma,
+                                                   self.sigma_lm, self.sigma_prior, self.uniform_pose_prior,
+                                                   self.sigma_trans, self.sigma_rot)
 
         self.posterior = posterior
 
@@ -236,6 +241,7 @@ class PDMMetropolisSampler:
         probabilities = torch.min(ratio, torch.ones_like(ratio))
         randoms = torch.rand(self.batch_size, device=self.dev)
         decider = torch.gt(probabilities, randoms)
+        # decider = torch.ones_like(decider)
         self.posterior = torch.where(decider, self.posterior, self.old_posterior)
         self.proposal.update(decider, self.posterior)
         self.batch_mesh.update_points(decider)
@@ -316,6 +322,7 @@ class PDMMetropolisSampler:
         ratio_rot = float(self.accepted_rot) / (self.accepted_rot + self.rejected_rot)
         ratio_tot = float(accepted_tot) / (accepted_tot + rejected_tot)
         return ratio_par, ratio_trans, ratio_rot, ratio_tot
+        # return ratio_par, None, None, ratio_tot
 
     def change_device(self, dev):
         """

src/visualization/DashViewer.py
@@ -237,10 +237,10 @@ class ChainVisualizer:
                     dcc.Slider(
                         id='chain-slider',
                         min=0,
-                        max=self.sampler.proposal.chain_length - 1,
+                        max=self.sampler.proposal.chain_length,
                         step=1000,
                         value=0,
-                        marks={**{i: str(i) for i in range(999, self.sampler.proposal.chain_length + 1, 1000)},
+                        marks={**{i: str(i) for i in range(1000, self.sampler.proposal.chain_length + 1, 1000)},
                                **{0: '0'}}
                     )
                 ], style={'width': '100%', 'padding': '10px'})
@@ -257,16 +257,22 @@ class ChainVisualizer:
             prevent_initial_call=True
         )
         def update_display(batch, chain_element):
-            params = self.parameters[:, batch, chain_element]
-            translation = params[-6:-3]
-            rotation = params[-3:]
-            points = self.sampler.model.get_points_from_parameters(params[:-6])
-            new_mesh = meshio.Mesh(points.numpy(), [meshio.CellBlock('triangle',
-                                                                     self.sampler.batch_mesh.cells[
+            params = self.parameters[:, batch, chain_element - 1]
+            if chain_element > 0:
+                translation = params[-6:-3]
+                rotation = params[-3:]
+                points = self.sampler.model.get_points_from_parameters(params[:-6])
+                new_mesh = meshio.Mesh(points.numpy(), [meshio.CellBlock('triangle', self.sampler.batch_mesh.cells[
                                                                          0].data.numpy())])
-            new_torch_mesh = TorchMeshGpu(new_mesh, 'display', torch.device("cpu"))
-            new_torch_mesh.apply_rotation(rotation)
-            new_torch_mesh.apply_translation(translation)
+                new_torch_mesh = TorchMeshGpu(new_mesh, 'display', torch.device("cpu"))
+                new_torch_mesh.apply_rotation(rotation)
+                new_torch_mesh.apply_translation(translation)
+            else:
+                points = self.sampler.model.get_points_from_parameters(torch.zeros_like(params[:-6]))
+                new_mesh = meshio.Mesh(points.numpy(), [meshio.CellBlock('triangle', self.sampler.batch_mesh.cells[
+                    0].data.numpy())])
+                new_torch_mesh = TorchMeshGpu(new_mesh, 'display', torch.device("cpu"))
+
             x, y, z = new_torch_mesh.tensor_points.T
             i, j, k = new_torch_mesh.cells[0].data.T
 