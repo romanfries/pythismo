- Additional calculation of the between-chain variance - Improved removal of wrong correspondences during the calculation of the analytic posterior - Batched PDM now has the option of having the same covariance for all models. In this case, the runtime is greatly improved as only one SVD has to be performed - Bugfix: Residual is now no longer incorrectly normalised - The framework now works for the assumption of fixed correspondences

src/analysis/Analyser.py
@@ -174,7 +174,9 @@ class ChainAnalyser:
         :return: Tensor containing the variances mentioned above with shape (num_points, batch_size).
         :rtype: torch.Tensor
         """
-        return self.mesh_chain[:, :, :, self.burn_in:].var(dim=3).mean(dim=1)
+        within = self.mesh_chain[:, :, :, self.burn_in:].var(dim=3).mean(dim=1)
+        between = (self.chain_length - self.burn_in) * self.mesh_chain[:, :, :, self.burn_in:].mean(dim=3).var(dim=2).mean(dim=1)
+        return within, between
 
     def posterior_analytics(self):
         """
@@ -237,13 +239,14 @@ class ChainAnalyser:
         :rtype: str
         """
         not_converged = self.simple_convergence_check()
+        # not_converged = torch.zeros((self.batch_size,), dtype=torch.bool, device=self.param_chain.device)
         means, mins, maxs, vars = self.posterior_analytics()
         rhat = self.rhat(ix=ix)
         ess = self.ess(ix=ix)
         mean_dist_c_post, mean_dist_n_post, squared_c_post, squared_n_post = self.mean_dist_to_target_post()
         mean_dist_c_map, mean_dist_n_map, squared_c_map, squared_n_map = self.mean_dist_to_target_map()
         hausdorff_avg, hausdorff_map = self.hausdorff_distances()
-        avg_var = self.avg_variance_per_point()
+        avg_var_within, avg_var_between = self.avg_variance_per_point()
         acc_par, acc_rnd, acc_trans, acc_rot, acc_tot = self.sampler.acceptance_ratio(~not_converged)
         data = {
             'description': 'MCMC statistics',
@@ -268,7 +271,8 @@ class ChainAnalyser:
                 'squared_clp_map': squared_n_map.tolist(),
                 'hausdorff_avg': hausdorff_avg.tolist(),
                 'hausdorff_map': hausdorff_map,
-                'var': avg_var.tolist()
+                'var': avg_var_within.tolist(),
+                'var_between': avg_var_between.tolist()
             },
             'unnormalised_log_density_posterior': {
                 'mean': means.tolist(),

src/main.py
@@ -2,6 +2,7 @@ import copy
 import math
 import os
 import sys
+from operator import attrgetter
 from pathlib import Path
 
 import itertools
@@ -23,7 +24,7 @@ from src.model import PointDistributionModel, distance_to_closest_point
 from src.registration import ICPAnalyser
 from src.sampling import PDMMetropolisSampler, create_target_aware_model, create_target_unaware_model
 from src.sampling.proposals import ClosestPointProposal
-from src.sampling.proposals.GaussRandWalk import ParameterProposalType
+from src.sampling.proposals.GaussRandWalk import ParameterProposalType, GaussianRandomWalkProposal
 from src.visualization import MainVisualizer
 
 # TODO: Create a Python package to avoid absolute imports
@@ -32,7 +33,7 @@ from src.visualization import MainVisualizer
 # https://stackoverflow.com/questions/62304087/installing-pytorch3d-fails-with-anaconda-and-pip-on-windows-10
 
 DECIMATE_MESHES = False
-RUN_ON_SCICORE_CLUSTER = True
+RUN_ON_SCICORE_CLUSTER = False
 RUN_WHOLE_EXPERIMENT = True
 GENERATE_PLOTS = False
 # Only relevant, if GENERATE_PLOTS is True. If True, plots are generated that show the average point wise
@@ -46,34 +47,39 @@ if RUN_ON_SCICORE_CLUSTER:
 else:
     DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-NUM_GPUS = 4
+NUM_GPUS = 1
 GPU_IDENTIFIERS = list(range(NUM_GPUS))
 
 REL_PATH_MESH = "datasets/femur-data/project-data/registered"
-REL_PATH_MESH_DECIMATED = "datasets/femur-data/project-data/registered-decimated"
-REL_PATH_INPUT_OUTPUT = "datasets/femur-data/project-data/output/unaware-std-reg"
+REL_PATH_MESH_DECIMATED = "datasets/femur-data/project-data/registered-decimated-1000"
+REL_PATH_INPUT_OUTPUT = "datasets/femur-data/project-data/output/test-2"
 
 DISTAL_END = True
-MODEL_TARGET_AWARE = False
-LAPLACIAN_TYPE = "std"
-ALPHA = 2
+ASSUME_FIXED_CORRESPONDENCES = False
+MODEL_TARGET_AWARE = True
+LAPLACIAN_TYPE = "none"
+ALPHA = 1
 BETA = 0.3
 
-BATCH_SIZE = 50
-CHAIN_LENGTH = 45000
+BATCH_SIZE = 10
+CHAIN_LENGTH = 10000
 DEFAULT_BURN_IN = 5000
-DECIMATION_TARGET = 200
+DECIMATION_TARGET = 1000
 
 MODEL_INFORMED_PROBABILITY = 0.5
 MODEL_RANDOM_PROBABILITY = 0.1
 TRANSLATION_PROBABILITY = 0.2
 ROTATION_PROBABILITY = 0.2
 
-VAR_MOD_RANDOM = torch.tensor([0.06, 0.12, 0.24], device=DEVICE)
-VAR_MOD_INFORMED = torch.tensor([0.12, 0.24, 0.48], device=DEVICE)
-VAR_TRANS = torch.tensor([0.2, 0.4, 0.8], device=DEVICE)
+VAR_MOD_RANDOM = torch.tensor([0.1, 0.2, 0.4], device=DEVICE)
+# VAR_MOD_RANDOM = torch.tensor([0.05, 0.1, 0.2], device=DEVICE)
+VAR_MOD_INFORMED = torch.tensor([0.16, 0.32, 0.64], device=DEVICE)
+# VAR_MOD_INFORMED = torch.tensor([0.12, 0.24, 0.48], device=DEVICE)
+VAR_TRANS = torch.tensor([0.25, 0.5, 1.0], device=DEVICE)
+# VAR_TRANS = torch.tensor([0.25, 0.5, 1.0], device=DEVICE)
 # Variance in radians
-VAR_ROT = torch.tensor([0.0015, 0.003, 0.006], device=DEVICE)
+VAR_ROT = torch.tensor([0.002, 0.004, 0.008], device=DEVICE)
+# VAR_ROT = torch.tensor([0.002, 0.004, 0.008], device=DEVICE)
 PROB_MOD_RANDOM = PROB_MOD_INFORMED = PROB_TRANS = PROB_ROT = torch.tensor([0.2, 0.6, 0.2], device=DEVICE)
 
 UNIFORM_POSE_PRIOR = False
@@ -103,8 +109,9 @@ def simplify_registered():
 def plot():
     os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
     handler = DataHandler(REL_PATH_INPUT_OUTPUT)
+    # handler.rename_files()
     data_dict = handler.read_all_statistics()
-    handler.generate_plots(data_dict, SEPARATE_PLOTS)
+    handler.generate_plots(4, data_dict, SEPARATE_PLOTS)
 
 
 def trial():
@@ -130,13 +137,13 @@ def trial():
     # z_min, z_max = torch.min(target.tensor_points, dim=0)[1][2].item(), \
     #     torch.max(target.tensor_points, dim=0)[1][2].item()
     # Manually determined landmarks
-    z_min, z_max = 184, 2
+    z_min, z_max = 876, 9
     part_target, plane_normal, plane_origin = target.partial_shape(z_max, z_min, obs, distal_end)
     del meshes[loo]
     if MODEL_TARGET_AWARE:
-        model = create_target_aware_model(meshes, DEVICE, target, part_target)
+        model, fid, bc = create_target_aware_model(meshes, DEVICE, target, part_target)
     else:
-        model = create_target_unaware_model(meshes, target)
+        model, fid, bc = create_target_unaware_model(meshes, DEVICE, part_target)
     shape = meshes[0]
     # dists = distance_to_closest_point(target.tensor_points.unsqueeze(-1), part_target.tensor_points, 1)
     # observed = (dists < 1e-6).squeeze()
@@ -157,11 +164,16 @@ def trial():
                                     var_mod_random, var_mod_informed, var_trans, var_rot, PROB_MOD_RANDOM,
                                     PROB_MOD_INFORMED, PROB_TRANS, PROB_ROT, var_n=var_likelihood, d=ICP_D,
                                     recalculation_period=ICP_RECALCULATION_PERIOD)
-
-    sampler = PDMMetropolisSampler(model, proposal, batched_shape, batched_target, LAPLACIAN_TYPE, ALPHA, BETA,
-                                   correspondences=False, gamma=GAMMA, var_like=var_likelihood,
-                                   uniform_pose_prior=UNIFORM_POSE_PRIOR, var_prior_trans=var_prior_trans,
-                                   var_prior_rot=var_prior_rot)
+    if ASSUME_FIXED_CORRESPONDENCES:
+        sampler = PDMMetropolisSampler(model, proposal, batched_shape, batched_target, LAPLACIAN_TYPE, ALPHA, BETA,
+                                       fixed_correspondences=True, triangles=fid, barycentric_coords=bc,
+                                       gamma=GAMMA, var_like=var_likelihood, uniform_pose_prior=UNIFORM_POSE_PRIOR,
+                                       var_prior_trans=var_prior_trans, var_prior_rot=var_prior_rot)
+    else:
+        sampler = PDMMetropolisSampler(model, proposal, batched_shape, batched_target, LAPLACIAN_TYPE, ALPHA, BETA,
+                                       fixed_correspondences=False, gamma=GAMMA, var_like=var_likelihood,
+                                       uniform_pose_prior=UNIFORM_POSE_PRIOR, var_prior_trans=var_prior_trans,
+                                       var_prior_rot=var_prior_rot)
 
     generator = torch.Generator(device=DEVICE)
     for _ in tqdm(range(CHAIN_LENGTH)):
@@ -179,7 +191,7 @@ def trial():
         sampler.decide(proposal_type, target)
     proposal.close()
     # batched_full_target = BatchTorchMesh(target, 'target', torch.device("cpu"), BATCH_SIZE)
-
+    # batched_shape.set_points(torch.stack(list(map(attrgetter('tensor_points'), meshes_))).permute(1, 2, 0))
     batched_shape.change_device(torch.device("cpu"))
     model.change_device(torch.device("cpu"))
     sampler.change_device(torch.device("cpu"))
@@ -207,17 +219,17 @@ def loocv():
         var_prior_trans = VAR_PRIOR_TRANS
         var_prior_rot = VAR_PRIOR_ROT
         for percentage in PERCENTAGES_OBSERVED_LENGTH:
-            for l_ in range(len(meshes)):
+            for l_ in range(1, len(meshes)):
                 # for l_ in range(10):
                 meshes_ = copy.deepcopy(meshes)
                 target = meshes_[l_]
-                z_min, z_max = 184, 2
+                z_min, z_max = 876, 9
                 part_target, plane_normal, plane_origin = target.partial_shape(z_max, z_min, percentage, DISTAL_END)
                 del meshes_[l_]
                 if MODEL_TARGET_AWARE:
-                    model = create_target_aware_model(meshes_, DEVICE, target, part_target)
+                    model, fid, bc = create_target_aware_model(meshes_, DEVICE, target, part_target)
                 else:
-                    model = create_target_unaware_model(meshes_, target)
+                    model, fid, bc = create_target_unaware_model(meshes_, DEVICE, part_target)
                 shape = meshes_[0]
                 dists = distance_to_closest_point(target.tensor_points.unsqueeze(-1), part_target.tensor_points, 1)
                 observed = (dists < 1e-6).squeeze()
@@ -240,11 +252,21 @@ def loocv():
                                                 var_n=var_likelihood, d=ICP_D,
                                                 recalculation_period=ICP_RECALCULATION_PERIOD)
 
-                sampler = PDMMetropolisSampler(model, proposal, batched_shape, batched_target, LAPLACIAN_TYPE, ALPHA,
-                                               BETA, correspondences=False, gamma=GAMMA, var_like=var_likelihood,
-                                               uniform_pose_prior=UNIFORM_POSE_PRIOR, var_prior_trans=var_prior_trans,
-                                               var_prior_rot=var_prior_rot, save_full_mesh_chain=True,
-                                               save_residuals=True)
+                if ASSUME_FIXED_CORRESPONDENCES:
+                    sampler = PDMMetropolisSampler(model, proposal, batched_shape, batched_target, LAPLACIAN_TYPE,
+                                                   ALPHA, BETA,
+                                                   fixed_correspondences=True, triangles=fid, barycentric_coords=bc,
+                                                   gamma=GAMMA, var_like=var_likelihood,
+                                                   uniform_pose_prior=UNIFORM_POSE_PRIOR,
+                                                   var_prior_trans=var_prior_trans, var_prior_rot=var_prior_rot, save_full_mesh_chain=True,
+                                                   save_residuals=True)
+                else:
+                    sampler = PDMMetropolisSampler(model, proposal, batched_shape, batched_target, LAPLACIAN_TYPE,
+                                                   ALPHA, BETA,
+                                                   fixed_correspondences=False, gamma=GAMMA, var_like=var_likelihood,
+                                                   uniform_pose_prior=UNIFORM_POSE_PRIOR,
+                                                   var_prior_trans=var_prior_trans,
+                                                   var_prior_rot=var_prior_rot, save_full_mesh_chain=True, save_residuals=True)
 
                 generator = torch.Generator(device=DEVICE)
                 for _ in tqdm(range(CHAIN_LENGTH)):
@@ -280,7 +302,7 @@ def loocv():
                 handler.save_target_avg(target, avg_var, l_, int(100 * percentage),
                                              int(100 * var_likelihood), save_html=True)
                 # Delete everything that is potentially located on the GPU
-                del analyser, avg_var, batched_shape, batched_target, dists, mean_dist_c_map, model, observed, part_target, plane_normal, plane_origin, proposal, sampler, shape, starting_params, starting_rotation, starting_translation, target
+                del analyser, avg_var, batched_shape, batched_target, bc, dists, fid, mean_dist_c_map, model, observed, part_target, plane_normal, plane_origin, proposal, sampler, shape, starting_params, starting_rotation, starting_translation, target
                 torch.cuda.empty_cache()
                 # Code to save further data
                 # chain_residual_dict = sampler.get_dict_chain_and_residuals()
@@ -324,9 +346,9 @@ def mcmc_task(gpu_id_, chunk_):
         part_target, plane_normal, plane_origin = target.partial_shape(z_max, z_min, percentage, DISTAL_END)
         del meshes_[l_]
         if MODEL_TARGET_AWARE:
-            model = create_target_aware_model(meshes_, device_, target, part_target)
+            model, fid, bc = create_target_aware_model(meshes_, device_, target, part_target)
         else:
-            model = create_target_unaware_model(meshes_, target)
+            model, fid, bc = create_target_unaware_model(meshes_, device_, part_target)
         shape = meshes_[0]
         dists = distance_to_closest_point(target.tensor_points.unsqueeze(-1), part_target.tensor_points, 1)
         observed = (dists < 1e-6).squeeze()
@@ -348,11 +370,17 @@ def mcmc_task(gpu_id_, chunk_):
                                         prob_trans, prob_rot, var_n=var_likelihood, d=ICP_D,
                                         recalculation_period=ICP_RECALCULATION_PERIOD)
 
-        sampler = PDMMetropolisSampler(model, proposal, batched_shape, batched_target, LAPLACIAN_TYPE, ALPHA, BETA,
-                                       correspondences=False, gamma=GAMMA, var_like=var_likelihood,
-                                       uniform_pose_prior=UNIFORM_POSE_PRIOR, var_prior_trans=var_prior_trans,
-                                       var_prior_rot=var_prior_rot, save_full_mesh_chain=True,
-                                       save_residuals=True)
+        if ASSUME_FIXED_CORRESPONDENCES:
+            sampler = PDMMetropolisSampler(model, proposal, batched_shape, batched_target, LAPLACIAN_TYPE, ALPHA, BETA,
+                                           fixed_correspondences=True, triangles=fid, barycentric_coords=bc,
+                                           gamma=GAMMA, var_like=var_likelihood, uniform_pose_prior=UNIFORM_POSE_PRIOR,
+                                           var_prior_trans=var_prior_trans, var_prior_rot=var_prior_rot, save_full_mesh_chain=True,
+                                           save_residuals=True)
+        else:
+            sampler = PDMMetropolisSampler(model, proposal, batched_shape, batched_target, LAPLACIAN_TYPE, ALPHA, BETA,
+                                           fixed_correspondences=False, gamma=GAMMA, var_like=var_likelihood,
+                                           uniform_pose_prior=UNIFORM_POSE_PRIOR, var_prior_trans=var_prior_trans,
+                                           var_prior_rot=var_prior_rot, save_full_mesh_chain=True, save_residuals=True)
 
         generator = torch.Generator(device=device_)
         for _ in range(CHAIN_LENGTH):
@@ -386,7 +414,7 @@ def mcmc_task(gpu_id_, chunk_):
         handler.save_target_avg(target, avg_var, l_, int(100 * percentage), int(100 * var_likelihood), save_html=True)
 
         # Delete everything that is potentially located on the GPU
-        del analyser, avg_var, batched_shape, batched_target, dists, mean_dist_c_map, model, observed, part_target, plane_normal, plane_origin, proposal, sampler, shape, starting_params, starting_rotation, starting_translation, target
+        del analyser, avg_var, batched_shape, batched_target, bc, dists, fid, mean_dist_c_map, model, observed, part_target, plane_normal, plane_origin, proposal, sampler, shape, starting_params, starting_rotation, starting_translation, target
         torch.cuda.empty_cache()
         # Code to save further data
         # chain_residual_dict = sampler.get_dict_chain_and_residuals()
@@ -404,10 +432,6 @@ if __name__ == "__main__":
             p = (Path.cwd().parent / Path(REL_PATH_MESH_DECIMATED)).glob('**/*')
             mesh_list = [f for f in p if f.is_file()]
             tasks = list(itertools.product(VAR_LIKELIHOOD_TERM, PERCENTAGES_OBSERVED_LENGTH, range(len(mesh_list))))
-            # tasks = [(a, b, c) for a, b, c in
-            #         itertools.product(VAR_LIKELIHOOD_TERM, PERCENTAGES_OBSERVED_LENGTH, range(len(mesh_list)))
-            #         if (b == 0.2 and (c + 2) % 4 == 0) or (b == 0.4 and (c + 1) % 4 == 0) or (
-            #         b == 0.6 and c % 4 == 0) or (b == 0.8 and (c + 3) % 4 == 0)]
             chunks = [tasks[i::NUM_GPUS] for i in range(NUM_GPUS)]
             processes = []
             for gpu_id, chunk in zip(GPU_IDENTIFIERS, chunks):

src/model/PointDistribution.py
@@ -569,14 +569,14 @@ class PointDistributionModel:
         :return: Said BatchedPointDistributionModel instance.
         :rtype: BatchedPointDistributionModel
         """
-        return BatchedPointDistributionModel(batch_size=batch_size, mean_and_cov=True,
+        return BatchedPointDistributionModel(batch_size=batch_size, identical_cov=True, mean_and_cov=True,
                                              mean=self.mean.expand(-1, batch_size),
                                              cov=self.get_covariance().unsqueeze(-1).expand(-1, -1, batch_size),
                                              rank=self.rank)
 
 
 class BatchedPointDistributionModel(PointDistributionModel):
-    def __init__(self, batch_size, read_in=False, mean_and_cov=False, meshes=None, mean=None, cov=None, model=None,
+    def __init__(self, batch_size, identical_cov=False, read_in=False, mean_and_cov=False, meshes=None, mean=None, cov=None, model=None,
                  rank=None, dev=None):
         """
         Class that defines and creates a batched point distribution model (PDM), i.e., 'batch_size' many different PDMs
@@ -613,7 +613,9 @@ class BatchedPointDistributionModel(PointDistributionModel):
         self.batch_size = batch_size
         self.read_in = read_in
         self.mean_and_cov = mean_and_cov
+        self.identical_cov = identical_cov
         if self.read_in:
+            # TODO: Reading in batched PDMs has never been carried out and tested
             self.meshes = None
             self.stacked_points = None
             # self.mean = (model.get('points').reshape(-1, order='F') + model.get('mean'))[:, np.newaxis]
@@ -639,12 +641,19 @@ class BatchedPointDistributionModel(PointDistributionModel):
             self.points_centered = None
             self.num_points = int(self.mean.shape[0] / 3)
             self.rank = rank
-            eigenvalues, self.eigenvectors = apply_batch_svd(cov, self.rank)
+            if self.identical_cov:
+                eigenvalues, self.eigenvectors = apply_svd(cov[:, :, 0], self.rank)
+                eigenvalues = eigenvalues.unsqueeze(-1).expand(-1, self.batch_size)
+                self.eigenvectors = self.eigenvectors.unsqueeze(-1).expand(-1, -1, self.batch_size)
+            else:
+                eigenvalues, self.eigenvectors = apply_batch_svd(cov, self.rank)
             self.eigenvalues = torch.sqrt((self.rank - 1) * eigenvalues)
             self.components = self.eigenvectors * torch.sqrt(self.eigenvalues).view(1, self.rank, -1)
             self.parameters = None
             self.decimated = False
         else:
+            # It is not possible to create different PDMs using the same set of meshes as a base
+            self.identical_cov = True
             self.meshes = meshes
             self.stacked_points = extract_points(self.meshes)
             self.dev = self.stacked_points.device
@@ -657,7 +666,9 @@ class BatchedPointDistributionModel(PointDistributionModel):
             self.num_points = int(self.stacked_points.size()[0] / 3)
             self.rank = self.stacked_points.size()[1]
             # Eigenvectors are the columns of the 2-dimensional ndarray 'self.eigenvectors'
-            self.eigenvalues, self.eigenvectors = apply_batch_svd(self.points_centered, self.rank)
+            self.eigenvalues, self.eigenvectors = apply_svd(self.points_centered[:, :, 0], self.rank)
+            self.eigenvalues = self.eigenvalues.unsqueeze(-1).expand(-1, self.batch_size)
+            self.eigenvectors = self.eigenvectors.unsqueeze(-1).expand(-1, -1, self.batch_size)
             self.components = self.eigenvectors * torch.sqrt(self.eigenvalues).view(1, self.rank, -1)
             # self.parameters = get_parameters(self.points_centered, self.eigenvectors)
             self.parameters = get_batch_parameters(self.points_centered[:, :, :], self.components)

src/registration/Procrustes.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 import torch
 from operator import attrgetter
 
@@ -23,37 +25,64 @@ def prepare_landmark_json(reference, landmarks_set):
     return reference_list, coordinates_list, identifier_list
 
 
+class PCAMode(Enum):
+    SINGLE = 1
+    BATCHED = 2
+
+
 class ProcrustesAnalyser:
-    def __init__(self, meshes, iterations=20):
-        # Important: Only functions as desired if correspondences are given and should therefore be used with
+    def __init__(self, meshes, references=None, iterations=20, mode=PCAMode.SINGLE):
+        # Important: Only functions as desired if fixed_correspondences are given and should therefore be used with
         # caution!
-        self.meshes = meshes
-        self.points = torch.stack(list(map(attrgetter('tensor_points'), self.meshes)))
+        self.mode = mode
         self.iterations = iterations
+
+        self.meshes = meshes
+        self.references = references
+        if self.mode == PCAMode.SINGLE:
+            self.points = torch.stack(list(map(attrgetter('tensor_points'), self.meshes)))
+        elif self.mode == PCAMode.BATCHED:
+            self.points = self.meshes.tensor_points.permute(2, 0, 1)
         self.transformation = None
 
     def generalised_procrustes_alignment(self, calc_facet_normals=False):
-        # Algorithm as outlined in the lecture notes of the course "Statistical Shape Modelling" by Marcel Lüthi
-        # 1: Choose a shape as the reference shape \Gamma_R
-        reference = self.points[0, :, :]
-        # 2: Align all shapes \Gamma^{(1)}, ..., \Gamma^{(n)} using the Procrustes alignment of two point sets
-        reference = reference.repeat(self.points.size(0), 1, 1)
-        rotations, translations, _ = pytorch3d.ops.corresponding_points_alignment(self.points, reference)
-        self.points = self.points @ rotations + translations.unsqueeze(1)
-        self.transformation = rotations, translations
-        # The initial reference is not used any further!
-
-        # 3: Compute the mean shape \Gamma_{\mu} for the set of aligned shapes
-        for _ in range(self.iterations):
-            mean = torch.mean(self.points, dim=0)
-
-            # 4: Iterate using the mean shape as the new reference shape
-            reference = mean.repeat(self.points.size(0), 1, 1)
+        if self.mode == PCAMode.SINGLE:
+            # Algorithm as outlined in the lecture notes of the course "Statistical Shape Modelling" by Marcel Lüthi
+            # 1: Choose a shape as the reference shape \Gamma_R
+            reference = self.points[0, :, :]
+            # 2: Align all shapes \Gamma^{(1)}, ..., \Gamma^{(n)} using the Procrustes alignment of two point sets
+            reference = reference.repeat(self.points.size(0), 1, 1)
             rotations, translations, _ = pytorch3d.ops.corresponding_points_alignment(self.points, reference)
             self.points = self.points @ rotations + translations.unsqueeze(1)
-            self.transformation = self.transformation[0] @ rotations, (self.transformation[1].unsqueeze(1) @ rotations + translations.unsqueeze(1)).squeeze()
-        # Save the changes in the original meshes
-        if calc_facet_normals:
-            _ = list(map(lambda x, y: x.set_points(y, adjust_rotation_centre=True), self.meshes, self.points))
-        else:
-            _ = list(map(lambda x, y: x.set_points(y, adjust_rotation_centre=True, calc_facet_normals=False), self.meshes, self.points))
+            self.transformation = rotations, translations
+            # The initial reference is not used any further!
+
+            # 3: Compute the mean shape \Gamma_{\mu} for the set of aligned shapes
+            for _ in range(self.iterations):
+                mean = torch.mean(self.points, dim=0)
+
+                # 4: Iterate using the mean shape as the new reference shape
+                reference = mean.repeat(self.points.size(0), 1, 1)
+                rotations, translations, _ = pytorch3d.ops.corresponding_points_alignment(self.points, reference)
+                self.points = self.points @ rotations + translations.unsqueeze(1)
+                self.transformation = self.transformation[0] @ rotations, (
+                            self.transformation[1].unsqueeze(1) @ rotations + translations.unsqueeze(1)).squeeze()
+            # Save the changes in the original meshes
+            if calc_facet_normals:
+                _ = list(map(lambda x, y: x.set_points(y, adjust_rotation_centre=True), self.meshes, self.points))
+            else:
+                _ = list(
+                    map(lambda x, y: x.set_points(y, adjust_rotation_centre=True, calc_facet_normals=False), self.meshes,
+                        self.points))
+
+    def procrustes_alignment(self):
+        if self.mode == PCAMode.BATCHED:
+            if self.meshes.tensor_points.shape[2] != self.references.tensor_points.shape[2]:
+                raise ValueError("In BATCHED mode, 'targets' and 'references' must have the same batch size.")
+
+            references_points = self.references.tensor_points.permute(2, 0, 1)
+            rotations, translations, _ = pytorch3d.ops.corresponding_points_alignment(self.points, references_points)
+            return rotations, translations
+
+
+

src/sampling/Metropolis.py
@@ -9,7 +9,7 @@ from pytorch3d.loss.point_mesh_distance import point_face_distance
 import point_cloud_utils as pcu
 
 from src.mesh.TMesh import get_transformation_matrix_from_rot_and_trans
-from src.model import PointDistributionModel
+from src.model import PointDistributionModel, BatchedPointDistributionModel
 from src.registration import ProcrustesAnalyser, ICPAnalyser
 from src.sampling.proposals import ParameterProposalType
 from src.sampling.proposals.ClosestPoint import extract_bounds_verts, is_point_on_line_segment
@@ -62,7 +62,7 @@ def unnormalised_log_posterior(distances, inv_cov_operator, parameters, translat
     :rtype: torch.Tensor
     """
     residual = torch.mul(distances, inv_cov_operator @ distances)
-    sigma_lm = (torch.linalg.vector_norm(inv_cov_operator @ distances) / torch.linalg.vector_norm(distances)) * sigma_lm
+    # sigma_lm = (torch.linalg.vector_norm(inv_cov_operator @ distances) / torch.linalg.vector_norm(distances)) * sigma_lm
     log_likelihoods = 0.5 * gamma * (torch.mean(-residual, dim=0) / sigma_lm)
     log_prior = 0.5 * torch.sum(-torch.pow(parameters / sigma_mod, 2), dim=0)
     rotation = (rotation + math.pi) % (2.0 * math.pi) - math.pi
@@ -118,7 +118,7 @@ def unnormalised_log_posterior_curvature(distances, inv_cov_operator, parameters
     :rtype: torch.Tensor
     """
     residual = torch.mul(distances, inv_cov_operator @ distances)
-    sigma_lm = (torch.linalg.vector_norm(inv_cov_operator @ distances) / torch.linalg.vector_norm(distances)) * sigma_lm
+    # sigma_lm = (torch.linalg.vector_norm(inv_cov_operator @ distances) / torch.linalg.vector_norm(distances)) * sigma_lm
     log_likelihoods = 0.5 * gamma * (torch.sum(torch.mul((-residual / sigma_lm), rel_curvs), dim=0))
     log_prior = 0.5 * torch.sum(-torch.pow(parameters / sigma_mod, 2), dim=0)
     rotation = (rotation + math.pi) % (2.0 * math.pi) - math.pi
@@ -156,24 +156,28 @@ def get_laplacian(type, verts, faces, edges, alpha, beta, dev):
         graph_laplacian = D @ graph_laplacian @ D
     if type == "eps":
         dists, idx, knn = pytorch3d.ops.ball_query(verts.unsqueeze(-1).permute(2, 0, 1),
-                                                   verts.unsqueeze(-1).permute(2, 0, 1), K=size, radius=20.0)
+                                                   verts.unsqueeze(-1).permute(2, 0, 1), K=size, radius=30.0)
         dists, idx = dists[0, :, :], idx[0, :, :]
         rows = torch.arange(dists.size(0), device=dev).unsqueeze(1).expand_as(dists)
         idx[idx == -1] = rows[idx == -1]
         graph_laplacian = torch.zeros((size, size), device=dev).scatter_(1, idx, dists)
         graph_laplacian = torch.where(graph_laplacian > 0, -torch.exp(-graph_laplacian / 900.0), graph_laplacian)
+        graph_laplacian = graph_laplacian / torch.norm(graph_laplacian, p=2, dim=1, keepdim=True)
+        graph_laplacian[torch.arange(graph_laplacian.size(0)), torch.arange(graph_laplacian.size(0))] = 1.0
 
-    prec = torch.matrix_power((identity + beta * graph_laplacian), alpha)
-    eigvals = (1.0 / (torch.pow(1 + beta * torch.linalg.eigh(graph_laplacian + 1e-6 * identity)[0], alpha) + 1e-6))
-    prec *= eigvals.mean()
-    det = -0.5 * torch.log(eigvals / eigvals.mean() + 1e-6).sum(-1) - (size / 2) * torch.log(torch.tensor(2 * torch.pi, device=dev))
-    return prec, eigvals, det
+    prec = torch.matrix_power((0.5 * identity + beta * graph_laplacian), alpha)
+    eigvals = (1.0 / (torch.pow(0.5 + beta * torch.linalg.eigh(graph_laplacian)[0], alpha) + 1e-6))
+    # prec *= eigvals.mean()
+    normalization = -0.5 * torch.log(eigvals / eigvals.mean() + 1e-6).sum(-1) - (size / 2) * torch.log(
+        torch.tensor(2 * torch.pi, device=dev))
+    return prec, eigvals, normalization
 
 
 class PDMMetropolisSampler:
-    def __init__(self, pdm, proposal, batch_mesh, target, laplacian_type, alpha=2, beta=0.3, correspondences=True, gamma=50.0, var_like=1.0,
-                 var_prior_mod=1.0, uniform_pose_prior=True, var_prior_trans=20.0, var_prior_rot=0.005,
-                 save_full_mesh_chain=False, save_residuals=False):
+    def __init__(self, pdm, proposal, batch_mesh, target, laplacian_type, alpha=2, beta=0.3, fixed_correspondences=True,
+                 triangles=None, barycentric_coords=None, gamma=50.0, var_like=1.0, var_prior_mod=1.0,
+                 uniform_pose_prior=True, var_prior_trans=20.0, var_prior_rot=0.005, save_full_mesh_chain=False,
+                 save_residuals=False):
         """
         Main class for Bayesian model fitting using Markov Chain Monte Carlo (MCMC). The Metropolis algorithm
         allows the user to draw samples from any distribution, given that the unnormalized distribution can be evaluated
@@ -190,9 +194,9 @@ class PDMMetropolisSampler:
         :param target: Observed (partial) shape to be analysed. Remark: It is required that the target has the same
         ‘batch_size’ as ‘batch_mesh’.
         :type target: BatchTorchMesh
-        :param correspondences: Boolean variable that determines whether there are known point correspondences between
+        :param fixed_correspondences: Boolean variable that determines whether there are known point fixed_correspondences between
         the model and the target.
-        :type correspondences: bool
+        :type fixed_correspondences: bool
         :param gamma: Multiplication factor of the likelihood term.
         :type gamma: float
         :param var_like: Variance used (in square millimetres) when determining the likelihood term of the posterior.
@@ -223,7 +227,9 @@ class PDMMetropolisSampler:
         self.points = self.batch_mesh.tensor_points
         self.target = target
         self.target_points = self.target.tensor_points
-        self.correspondences = correspondences
+        self.fixed_correspondences = fixed_correspondences
+        self.triangles = triangles
+        self.barycentric_coords = barycentric_coords
         self.batch_size = self.proposal.batch_size
         self.gamma = gamma
         self.var_like = var_like
@@ -262,7 +268,9 @@ class PDMMetropolisSampler:
         self.laplacian_type = laplacian_type
         self.alpha = alpha
         self.beta = beta
-        self.inv_cov_operator, eigvals, det = get_laplacian(self.laplacian_type, self.target_points[:, :, 0], self.target.cells[0].data, edges, self.alpha, self.beta, self.dev)
+        self.inv_cov_operator, eigvals, det = get_laplacian(self.laplacian_type, self.target_points[:, :, 0],
+                                                            self.target.cells[0].data, edges, self.alpha, self.beta,
+                                                            self.dev)
 
         self.posterior = None
         self.determine_quality(ParameterProposalType.MODEL_RANDOM)
@@ -332,10 +340,9 @@ class PDMMetropolisSampler:
         """
         self.update_mesh(parameter_proposal_type)
         self.old_posterior = self.posterior
-        if self.correspondences:
-            # TODO: Does not support partial targets. The number of points of the current mesh instances and the target
-            #  must be identical (equal to the full number of model points N).
-            differences = torch.sub(self.points, self.target_points)
+        if self.fixed_correspondences:
+            correspondences = (batch_interpolate_barycentric_coords(self.batch_mesh.cells[0].data.unsqueeze(0).expand(self.batch_size, -1, -1), self.triangles.unsqueeze(0).expand(self.batch_size, -1), self.barycentric_coords.unsqueeze(0).expand(self.batch_size, -1, -1), self.points.permute(2, 0, 1))).permute(1, 2, 0)
+            differences = torch.sub(self.target_points, correspondences)
             distances = torch.linalg.vector_norm(differences, dim=1)
             posterior = unnormalised_log_posterior(distances, self.inv_cov_operator, self.proposal.parameters,
                                                    self.proposal.translation, self.proposal.rotation, self.gamma,
@@ -483,12 +490,18 @@ class PDMMetropolisSampler:
             - Total ratio of accepted proposals.
         :rtype: tuple
         """
-        accepted_tot = torch.sum((self.accepted_par + self.accepted_rnd + self.accepted_trans + self.accepted_rot)[selector]).item()
-        rejected_tot = torch.sum((self.rejected_par + self.rejected_rnd + self.rejected_trans + self.rejected_rot)[selector]).item()
-        ratio_par = float(torch.sum(self.accepted_par[selector]).item()) / torch.sum((self.accepted_par + self.rejected_par)[selector]).item()
-        ratio_rnd = float(torch.sum(self.accepted_rnd[selector]).item()) / torch.sum((self.accepted_rnd + self.rejected_rnd)[selector]).item()
-        ratio_trans = float(torch.sum(self.accepted_trans[selector]).item()) / torch.sum((self.accepted_trans + self.rejected_trans)[selector]).item()
-        ratio_rot = float(torch.sum(self.accepted_rot[selector]).item()) / torch.sum((self.accepted_rot + self.rejected_rot)[selector]).item()
+        accepted_tot = torch.sum(
+            (self.accepted_par + self.accepted_rnd + self.accepted_trans + self.accepted_rot)[selector]).item()
+        rejected_tot = torch.sum(
+            (self.rejected_par + self.rejected_rnd + self.rejected_trans + self.rejected_rot)[selector]).item()
+        ratio_par = float(torch.sum(self.accepted_par[selector]).item()) / torch.sum(
+            (self.accepted_par + self.rejected_par)[selector]).item()
+        ratio_rnd = float(torch.sum(self.accepted_rnd[selector]).item()) / torch.sum(
+            (self.accepted_rnd + self.rejected_rnd)[selector]).item()
+        ratio_trans = float(torch.sum(self.accepted_trans[selector]).item()) / torch.sum(
+            (self.accepted_trans + self.rejected_trans)[selector]).item()
+        ratio_rot = float(torch.sum(self.accepted_rot[selector]).item()) / torch.sum(
+            (self.accepted_rot + self.rejected_rot)[selector]).item()
         ratio_tot = float(accepted_tot) / (accepted_tot + rejected_tot)
         return ratio_par, ratio_rnd, ratio_trans, ratio_rot, ratio_tot
         # return ratio_par, None, None, ratio_tot
@@ -545,45 +558,78 @@ def create_target_aware_model(meshes, dev, target, part_target):
     mean_shape.set_points(torch.mean(gpa.points, dim=0), adjust_rotation_centre=True)
     icp = ICPAnalyser([part_target, mean_shape])
     icp.icp(swap_transformation=True)
-    target.set_points((torch.matmul(target.tensor_points, icp.transformation[0]) + icp.transformation[1].unsqueeze(1)).squeeze(), adjust_rotation_centre=True)
+    target.set_points(
+        (torch.matmul(target.tensor_points, icp.transformation[0]) + icp.transformation[1].unsqueeze(1)).squeeze(),
+        adjust_rotation_centre=True)
 
-    # Estimate the correspondences
-    cloud_cpu = mean_shape.tensor_points.cpu().numpy()
-    mesh_cpu = part_target.tensor_points.cpu().numpy()
-    mesh_faces = part_target.cells[0].data.cpu().numpy()
-    dists, fid, bc = pcu.closest_points_on_mesh(cloud_cpu, mesh_cpu, mesh_faces)
-    closest_pts = pcu.interpolate_barycentric_coords(mesh_faces, fid, bc, mesh_cpu)
+    # Estimate the correspondences using model sampling
+    closest_pts, dists, _, _ = estimate_correspondences(mean_shape, part_target, dev)
 
     # Remove wrong correspondences by detecting the nearest points located on the mesh boundary
     # boundary_edges = trimesh.base.Trimesh(mesh_cpu, mesh_faces, process=True, validate=True).outline().vertex_nodes
     boundary_edges = extract_bounds_verts(part_target.cells[0].data, extract_edges=True)
-    observed = ~(is_point_on_line_segment(torch.tensor(closest_pts, device=dev), torch.tensor(mesh_cpu, device=dev)[boundary_edges], tol=1e-2))
+    observed = ~(
+        is_point_on_line_segment(closest_pts, part_target.tensor_points[boundary_edges],
+                                 tol=1e-2))
 
     # Align all meshes using the observed points
     meshes_observed_verts = copy.deepcopy(meshes)
     _ = list(map(lambda x, y: x.set_points(y.tensor_points[observed, :]), meshes_observed_verts, meshes))
     target_corresponding_verts = part_target.copy()
-    target_corresponding_verts.set_points(torch.tensor(closest_pts, device=dev)[observed])
+    target_corresponding_verts.set_points(closest_pts[observed])
     meshes_observed_verts.insert(0, target_corresponding_verts)
-
     gpa_target = ProcrustesAnalyser(meshes_observed_verts)
     gpa_target.generalised_procrustes_alignment()
-    # Apply the found transformations to all full meshes
-    transformations = get_transformation_matrix_from_rot_and_trans(gpa_target.transformation[0].permute(1, 2, 0), gpa_target.transformation[1].permute(1, 0))
+
+    # Apply the found transformations to full meshes
+    transformations = get_transformation_matrix_from_rot_and_trans(gpa_target.transformation[0].permute(1, 2, 0),
+                                                                   gpa_target.transformation[1].permute(1, 0))
     meshes.insert(0, part_target)
     _ = list(map(lambda x, y: x.apply_transformation(y), meshes, transformations.permute(2, 0, 1)))
     target.apply_transformation(transformations.permute(2, 0, 1)[0])
     del meshes[0]
 
+    # Estimate the correspondences using target sampling
+    _, _, fid, bc = estimate_correspondences(part_target, mean_shape, dev)
     model = PointDistributionModel(meshes=meshes)
-    return model
+    return model, fid, bc
 
 
-def create_target_unaware_model(meshes, target):
+def create_target_unaware_model(meshes, dev, target):
     ProcrustesAnalyser(meshes).generalised_procrustes_alignment()
     model = PointDistributionModel(meshes=meshes)
     mean_shape = meshes[0].copy()
     mean_shape.set_points(model.get_mean_points(), adjust_rotation_centre=True)
     ICPAnalyser([target, mean_shape]).icp()
     model.mean = mean_shape.tensor_points.reshape(3 * model.num_points, 1)
-    return model
+
+    # Estimate the correspondences using target sampling
+    _, _, fid, bc = estimate_correspondences(target, mean_shape, dev)
+
+    return model, fid, bc
+
+
+def estimate_correspondences(x, y, dev):
+    cloud_cpu = x.tensor_points.cpu().numpy()
+    mesh_cpu = y.tensor_points.cpu().numpy()
+    mesh_faces = y.cells[0].data.cpu().numpy()
+    dists, fid, bc = pcu.closest_points_on_mesh(cloud_cpu, mesh_cpu, mesh_faces)
+    closest_pts = pcu.interpolate_barycentric_coords(mesh_faces, fid, bc, mesh_cpu)
+    return torch.tensor(closest_pts, device=dev), torch.tensor(dists, device=dev), torch.tensor(fid, device=dev), \
+        torch.tensor(bc, device=dev)
+
+
+def interpolate_barycentric_coords(f, fi, bc, attribute):
+    f_vertices = f[fi]
+    f_attributes = attribute[f_vertices]
+    return (f_attributes * bc.unsqueeze(-1)).sum(dim=1)
+
+
+def batch_interpolate_barycentric_coords(f, fi, bc, attribute):
+    batch_size = f.shape[0]
+    batch_indices = torch.arange(batch_size, device=f.device).unsqueeze(-1)
+    batch_indices_fi = batch_indices.expand(-1, fi.shape[1])
+    f_vertices = f[batch_indices_fi, fi]
+    batch_indices_f = batch_indices.unsqueeze(-1).expand(-1, f_vertices.shape[1], f_vertices.shape[2])
+    f_attributes = attribute[batch_indices_f, f_vertices]
+    return (f_attributes * bc.unsqueeze(-1)).sum(dim=2)

src/sampling/proposals/ClosestPoint.py
@@ -1,3 +1,5 @@
+import copy
+
 import numpy as np
 import warnings
 
@@ -5,12 +7,14 @@ import meshio
 import torch
 from torch.autograd.function import once_differentiable
 
+import pytorch3d.transforms
 from pytorch3d import _C
 from pytorch3d.loss.point_mesh_distance import _PointFaceDistance, point_face_distance
 import point_cloud_utils as pcu
 
-from src.mesh import BatchTorchMesh, TorchMeshGpu
+from src.mesh import BatchTorchMesh, TorchMeshGpu, get_transformation_matrix_from_euler_angles
 from src.model import PointDistributionModel, BatchedPointDistributionModel
+from src.registration.Procrustes import PCAMode, ProcrustesAnalyser
 from src.sampling.proposals.GaussRandWalk import GaussianRandomWalkProposal, ParameterProposalType
 
 
@@ -200,6 +204,7 @@ class ClosestPointProposal(GaussianRandomWalkProposal):
         self.single_target.set_points(batched_target.tensor_points[:, :, 0].squeeze(), adjust_rotation_centre=True)
         self.batched_targets = batched_target
         self.partial_target = self.batched_targets.num_points < self.batched_shapes.num_points
+        self.observed, self.closest_pts = None, None
 
         self.var_mod_icp = var_mod_informed
         self.prob_mod_icp = prob_mod_informed
@@ -289,6 +294,7 @@ class ClosestPointProposal(GaussianRandomWalkProposal):
             boundary_edges = extract_bounds_verts(target_cells, extract_edges=True)
             on_boundary = is_point_on_line_segment(torch.tensor(closest_pts, device=self.dev),
                                                    torch.tensor(mesh_cpu, device=self.dev)[boundary_edges], tol=1e-2)
+        self.observed, self.closest_pts = ~on_boundary, torch.tensor(closest_pts, device=self.dev)
 
         # 3: Construct the set of observations L based on corresponding landmark pairs (s_i, c_i) according to eq. 9
         # and define the noise \epsilon_i \distas \mathcal{N}(0, \Sigma_{s_{i}} using eq. 16.
@@ -336,7 +342,7 @@ class ClosestPointProposal(GaussianRandomWalkProposal):
         cov_posterior = cov_prior - K_xy @ K_yy_inv @ torch.t(K_xy)
         # TODO: There is a faster method to calculate the posterior model that takes advantage of the fact that the
         #       model is only of low rank. This is implemented in Scalismo.
-        posterior_model = BatchedPointDistributionModel(self.batch_size, mean_and_cov=True,
+        posterior_model = BatchedPointDistributionModel(self.batch_size, identical_cov=True, mean_and_cov=True,
                                                         # mean=mean_posterior,
                                                         mean=batched_shape.tensor_points.reshape(
                                                             (3 * m, self.batch_size)),